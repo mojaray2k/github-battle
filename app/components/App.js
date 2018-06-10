778c04a8e9f1a9a027ed3633e6c2b0c679ef6922
Amen Ra <ara@Amens-MacBook-Pro.local>
2018-06-10 17:54:20 -0400

    adding React Router and building out the UI Containers

--- app/components/App.js
+++ app/components/App.js
@@ -1,14 +1,33 @@
 var React = require('react');
-var Popular = require('./Poplular')
+var Home = require('./Home');
+var Battle = require('./Battle');
+var Popular = require('./Poplular');
+var ReactRouter = require('react-router-dom');
+var Router = ReactRouter.BrowserRouter;
+var Route = ReactRouter.Route;
+var Switch = ReactRouter.Switch;
+var Nav = require('./Nav');
+
 
 class App extends React.Component {  
   render(){
     return (
-      <div className='container'>
-        <Popular />  
-      </div>
+      <Router>
+        <div className='container'>
+          <Nav />
+          <Switch>
+            <Route exact path='/' component={Home} />
+            <Route exact path='/battle' component={Battle} />
+            <Route path='/popular' component={Popular} />
+            <Route render={function() {
+              return <p>Not Found</p>
+            }} />
+          </Switch>
+        </div>
+      </Router>
+      
     )
   }
 }
 
-module.exports = App;
+module.exports = App; 
