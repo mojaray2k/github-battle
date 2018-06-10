5d9f57a549a67b4e4f3b672664a1818ef97cede4
Amen Ra <ara@Amens-MacBook-Pro.local>
2018-06-10 11:08:22 -0400

    created the popular array component and restructed app

--- app/index.js
+++ app/index.js
@@ -1,37 +1,9 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
-var PropTypes = require('prop-types')
+require('./index.css');
+var App = require('./components/App');
 
-// state
-// lifecycle event
-// UI
-
-class Badge extends React.Component {  
-  render() {
-    return (
-      <div>
-        <img 
-          src={this.props.img} 
-          alt="Avatar"
-          style={{width: 100, height: 100}}
-        />
-        <h1>Name: {this.props.name}</h1>
-        <h3>username: {this.props.username}</h3>
-      </div>
-    )
-  }
-}
-
-Badge.propTypes = {
-  img: PropTypes.string.isRequired,
-  name: PropTypes.string.isRequired,
-  username: PropTypes.string.isRequired
-}
 ReactDOM.render(
-  <Badge 
-    name='Amen Ra'
-    username='mojaray2k'
-    img='https://avatars3.githubusercontent.com/u/875558?s=460&v=4'
-  />,
+  <App />,
   document.getElementById('app')
 );
